9cedb652c77cfe027d3ec3d40258309de04c4a75
Alexandre Bléron <alex.bleron@gmail.com>
2018-12-25 19:06:07 +0100

    init

---
+++ examples/sample.rs
@@ -0,0 +1,14 @@
+use inconsistent_import::*;
+use image;
+use image::GenericImageView;
+
+// thread 'main' panicked at 'src\librustc_resolve\resolve_imports.rs:970: inconsistent resolution for an import', src\librustc\util\bug.rs:47:26
+//
+// if the glob import is replaced by `inconsistent_import::*`, the compiler (correctly) emits
+//      error[E0252]: the name `image` is defined multiple times
+//
+// if the glob import is moved after the two imports, the code compiles
+
+fn main()
+{
+}
